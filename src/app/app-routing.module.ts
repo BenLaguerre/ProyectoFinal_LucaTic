b29a47b4fbd4bbdc16430184904a303c58cdca9a
unknown <mariainfanteslacal@gmail.com>
2021-11-30 15:30:03 +0100

    creada ruta de seleccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPerfilComponent } from './components/add-perfil/add-perfil.component';
+import { SeleccionComponent } from './components/seleccion/seleccion.component';
 
 const routes: Routes = [
   {
     path: 'add-perfil',
     component: AddPerfilComponent
-  }
+  },
+  {
+    path: 'seleccion',
+    component: SeleccionComponent
+  },
+  
+
+  
 ];
 
 @NgModule({
