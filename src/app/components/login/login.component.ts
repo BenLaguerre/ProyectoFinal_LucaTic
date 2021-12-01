2062621f78f85ac2206277fa69ace01fe8768c89
Miguel Angel Hernandez <migle.hp@gmail.com>
2021-12-01 18:58:24 +0100

    cosas login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CrudService } from 'src/app/service/crud.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,4 +14,31 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  formLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  })
+
+  getErrorMessage() {
+    if (this.formLogin.get('email')?.hasError('required')) {
+      return 'email requerido';
+    }
+    return this.formLogin.get('email')?.hasError('email')
+      ? 'No es un email valido'
+      : '';
+  }
+
+  getErrorMessagePass() {
+    if (this.formLogin.get('password')?.dirty) {
+      return 'debe tener al menos 8 caracteres';
+    }
+    return this.formLogin.get('password')?.hasError('required')
+      ? 'contraseña requerida'
+      : '';
+  }
+  hide = true;
 }
+
