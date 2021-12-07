71069b002512e3a675062e8dc854a88b0d8f6aa4
unknown <mariainfanteslacal@gmail.com>
2021-12-07 13:42:38 +0100

    testeado crudService y seleecionService

--- src/app/service/crud.service.spec.ts
+++ src/app/service/crud.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CrudService } from './crud.service';
@@ -5,12 +6,50 @@ import { CrudService } from './crud.service';
 describe('CrudService', () => {
   let service: CrudService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CrudService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [CrudService]
+  }));
 
-  it('should be created', () => {
+   it('should be created', () => {
+    service= TestBed.get(CrudService);
     expect(service).toBeTruthy();
-  });
+   });
+
+   //comprobamos si el crudService tiene la función para listar perfiles
+   it('should have GetAllProfile function', () => {
+    service= TestBed.get(CrudService);
+    expect(service.GetAllProfile).toBeTruthy();
+   });
+
+   //comprobamos si el crudService tiene la función para añadir perfiles
+   it('should have addProfile function', () => {
+    service= TestBed.get(CrudService);
+    expect(service.addProfile).toBeTruthy();
+   });
+
+   //comprobamos si el crudService tiene la función para buscar un perfil
+   it('should have GetOnProfile function', () => {
+    service= TestBed.get(CrudService);
+    expect(service.GetOnProfile).toBeTruthy();
+   });
+
+   //comprobamos si el crudService tiene la función para traer un perfil aleatorio
+   it('should have SelectOnProfile function', () => {
+    service= TestBed.get(CrudService);
+    expect(service.SelectOnProfile).toBeTruthy();
+   });
+
+   //comprobamos si el crudService tiene la función para actualizar un perfil
+   it('should have updateProfile function', () => {
+    service= TestBed.get(CrudService);
+    expect(service.updateProfile).toBeTruthy();
+   });
+
+   //comprobamos si el crudService tiene la función para coger el email del perfil que se está mostrando
+   it('should have GetCurrentProfile function', () => {
+    service= TestBed.get(CrudService);
+    expect(service.GetCurrentProfile).toBeTruthy();
+   });
+
 });
