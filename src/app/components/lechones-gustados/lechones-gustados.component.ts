ad2f16a38d84a7e96238c10548f1a7bcffa80c87
Joseba Ibarlucea <Jibarlucea91@gmail.com>
2021-12-09 19:31:48 +0100

    Last touches

--- src/app/components/lechones-gustados/lechones-gustados.component.ts
+++ src/app/components/lechones-gustados/lechones-gustados.component.ts
@@ -5,47 +5,46 @@ import { AuthService } from 'src/app/service/auth.service';
 @Component({
   selector: 'app-lechones-gustados',
   templateUrl: './lechones-gustados.component.html',
-  styleUrls: ['./lechones-gustados.component.scss']
+  styleUrls: ['./lechones-gustados.component.scss'],
 })
 export class LechonesGustadosComponent implements OnInit {
-
-  constructor(private crudService: CrudService, public auth:AuthService) { }
+  constructor(private crudService: CrudService, public auth: AuthService) {}
   //Falta añadir el html y su logica :)
-  getStatus(){
-    let status = sessionStorage.getItem("status")
-    return status
+  getStatus() {
+    let status = sessionStorage.getItem('status');
+    return status;
   }
   userMail!: string;
   likes!: any;
   likesInfo: any[] = [];
-  Id!:number;
+  Id!: number;
 
   ngOnInit(): void {
-    setTimeout(()=>{
-      this.fetchLikes()
-      }, 500)
-      setTimeout(()=>{
-        this.showLiked()
-      }, 800)
+    setTimeout(() => {
+      this.fetchLikes();
+    }, 1000);
+    setTimeout(() => {
+      this.showLiked();
+    }, 1200);
   }
 
-  fetchLikes(): any{
-    this.userMail = this.auth.userFirebase
-    this.crudService.GetCurrentProfile(this.userMail).subscribe(res => {
-      res
-      this.likes = res[0].arrayLikes
-    })
+  fetchLikes(): any {
+    this.userMail = this.auth.userFirebase;
+    this.crudService.GetCurrentProfile(this.userMail).subscribe((res) => {
+      res;
+      this.likes = res[0].arrayLikes;
+    });
   }
-  showLiked():any{
-    this.likes.forEach( (userInfo: any) => {
-      this.crudService.GetCurrentProfile(userInfo).subscribe(users => {
-        this.likesInfo.push(users)
+  showLiked(): any {
+    this.likes.forEach((userInfo: any) => {
+      this.crudService.GetCurrentProfile(userInfo).subscribe((users) => {
+        this.likesInfo.push(users);
       });
     });
   }
-  ensenaId(Ide:number){
+  ensenaId(Ide: number) {
     this.Id = Ide;
     let stringID = Ide.toString();
-    sessionStorage.setItem("ide",stringID);
+    sessionStorage.setItem('ide', stringID);
   }
 }
